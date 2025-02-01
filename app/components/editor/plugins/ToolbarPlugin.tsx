2b0c7d095ea412f1ad35a54a0a09bacf3ab2ad5a
Yan Moe Naing <yan.webdev.1234@gmail.com>
2025-02-01 22:24:10 +0630

    check isText

--- app/components/editor/plugins/ToolbarPlugin.tsx
+++ app/components/editor/plugins/ToolbarPlugin.tsx
@@ -1,10 +1,39 @@
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
 import { TOGGLE_LINK_COMMAND } from "@lexical/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { $getSelection, $isRangeSelection, getDOMSelection } from "lexical";
 
 const ToolbarPlugin: React.FC = () => {
   const [editor] = useLexicalComposerContext();
   const [url, setUrl] = useState("");
+
+  useEffect(() => {
+    return editor.registerUpdateListener(({ editorState }) => {
+      editorState.read(() => {
+        const selection = $getSelection();
+        const nativeSelection = getDOMSelection(editor._window);
+        const rootElement = editor.getRootElement();
+
+        if (
+          nativeSelection !== null &&
+          (!$isRangeSelection(selection) ||
+            rootElement === null ||
+            !rootElement.contains(nativeSelection.anchorNode))
+        ) {
+          console.log("it is not text");
+          return;
+        }
+
+        if ($isRangeSelection(selection)) {
+          console.log("Range selection");
+        } else {
+          console.log("Not Range selection");
+        }
+
+        console.log(selection?.getTextContent());
+      });
+    });
+  }, [editor]);
 
   const addLink = () => {
     editor.dispatchCommand(TOGGLE_LINK_COMMAND, {
@@ -15,6 +44,11 @@ const ToolbarPlugin: React.FC = () => {
   const handleSubmit = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === "Enter") {
       e.preventDefault();
+      editor.read(() => {
+        const s = $getSelection();
+        console.log("before adding link", s?.getTextContent());
+        console.log("iscoll", s?.isCollapsed())
+      });
       addLink();
     }
   };
